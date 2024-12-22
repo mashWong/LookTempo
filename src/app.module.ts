b231ef5abaacc8dd2c3fa81b077e1efb96505e47
blast <1115310504@qq.com>
2024-12-23 04:33:56 +0800

    测试去除数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,25 +8,25 @@ import { LoggersController } from './controller/logger.controller';
 import { TwitterService } from './service/twitter.service';
 import { PayPalService } from './service/paypal.service';
 import { PaymentController } from './controller/pay.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './module/user.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './module/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '00000000',
-      database: 'looktempo',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    UserModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: '127.0.0.1',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '00000000',
+    //   database: 'looktempo',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
+    // UserModule,
   ],
-  controllers: [VideoController, AppController, LoggersController, PaymentController],
-  providers: [AppService, LoggerService, TwitterService, PayPalService],
+  controllers: [VideoController, AppController, LoggersController],
+  providers: [AppService, LoggerService, TwitterService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
